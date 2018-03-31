5aff3e06b8f71255ee9cd27bf5d351099b00fa57
Gianluca Bine <gian_bine@hotmail.com>
2018-03-30 22:58:11 -0300

    Correcao de indentacao

--- devops/issue-start.sh
+++ devops/issue-start.sh
@@ -5,12 +5,12 @@ git pull origin master
 
 # Branch Issue
 if [[ -n $(git branch | grep issue#$1) ]]; then
-  git checkout issue#$1
-  git pull origin master
-  git push --set-upstream origin issue#$1
+	git checkout issue#$1
+	git pull origin master
+	git push --set-upstream origin issue#$1
 else
-  git checkout -b issue-#$1
-  git push --set-upstream origin issue#$1
+	git checkout -b issue-#$1
+	git push --set-upstream origin issue#$1
 fi
 
 echo "Tudo pronto, agora no branch: issue#$1"
