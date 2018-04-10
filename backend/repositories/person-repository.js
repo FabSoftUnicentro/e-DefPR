ad6fce8fd83af0e8df28cbe230a42cdcde736b13
Gianluca Bine <gian_bine@hotmail.com>
2018-04-09 21:47:46 -0300

    Linter standard.js

--- backend/repositories/person-repository.js
+++ backend/repositories/person-repository.js
@@ -1,61 +1,61 @@
-'use strict';
+'use strict'
 
-const mongoose = require('mongoose');
-const Person = mongoose.model('Person');
+const mongoose = require('mongoose')
+const Person = mongoose.model('Person')
 
-exports.get = async() => {
-    const res = await Person.find({
-        active: true
-    }, 'type cpf rg name gender placeOfBirth maritalStatus profession salary serviceNumber dateOfBirth active');
-    
-    return res;
+exports.get = async () => {
+  const res = await Person.find({
+    active: true
+  }, 'type cpf rg name gender placeOfBirth maritalStatus profession salary serviceNumber dateOfBirth active')
+
+  return res
 }
 
-exports.getById = async(id) => {
-    const res = await Person.find({
-        _id: id,
-        active: true
-    }, 'type cpf rg name gender placeOfBirth maritalStatus profession salary serviceNumber dateOfBirth active');
+exports.getById = async (id) => {
+  const res = await Person.find({
+    _id: id,
+    active: true
+  }, 'type cpf rg name gender placeOfBirth maritalStatus profession salary serviceNumber dateOfBirth active')
 
-    return res;
+  return res
 }
 
-exports.create = async(data) => {
-    var Person = new Person(data);
+exports.create = async (data) => {
+  var Person = new Person(data)
 
-    await Person.save();
+  await Person.save()
 }
 
-exports.update = async(id, data) => {
-    await Person
-        .findByIdAndUpdate(id, {
-            $set: {
-                type: data.type,
-                cpf: data.cpf,
-                rg: data.rg,
-                name: data.name,
-                gender: data.gender,
-                placeOfBirth: data.placeOfBirth,
-                maritalStatus: data.maritalStatus,
-                password: data.password,
-                profession: data.profession,
-                serviceNumber: data.serviceNumber,
-                dateOfBirth: data.dateOfBirth,
-                active: data.active,
-            }
-        });
+exports.update = async (id, data) => {
+  await Person
+    .findByIdAndUpdate(id, {
+      $set: {
+        type: data.type,
+        cpf: data.cpf,
+        rg: data.rg,
+        name: data.name,
+        gender: data.gender,
+        placeOfBirth: data.placeOfBirth,
+        maritalStatus: data.maritalStatus,
+        password: data.password,
+        profession: data.profession,
+        serviceNumber: data.serviceNumber,
+        dateOfBirth: data.dateOfBirth,
+        active: data.active
+      }
+    })
 }
 
-exports.delete = async(id) => {
-    await Person
-        .findOneAndRemove(id);
+exports.delete = async (id) => {
+  await Person
+    .findOneAndRemove(id)
 }
 
-exports.authenticate = async(data) => {
-    const res = await Person.findOne({
-        email: data.email,
-        password: data.password
-    });
+exports.authenticate = async (data) => {
+  const res = await Person.findOne({
+    email: data.email,
+    password: data.password
+  })
 
-    return res;
+  return res
 }
