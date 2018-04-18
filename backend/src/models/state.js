50e6690a28024853eacbd4714934c926f9f3b913
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-04-17 21:19:07 -0300

    Implementado os models de city e state

---
+++ backend/src/models/state.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+  // _id
+  ibgeCode: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  initials: {
+      type: String,
+      required: true,
+  }
+})
+
+module.exports = mongoose.model('State', schema)
