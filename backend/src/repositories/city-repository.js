a5ae3755eff99508677b9e0122f052c1cee38b4f
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-04-17 21:19:30 -0300

    Implementado repositorios de city e state

---
+++ backend/src/repositories/city-repository.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const City = mongoose.model('City')
+
+exports.get = async () => {
+  const res = await City.find({
+  }, 'ibgeCode name ufCode latitude longitude')
+
+  return res
+}
+
+exports.getById = async (id) => {
+  const res = await City.findOne({
+    _id: id,
+  }, 'ibgeCode name ufCode latitude longitude')
+
+  return res
+}
+
+exports.create = async (data) => {
+  let newCity = new City(data)
+
+  await newCity.save()
+}
+
+exports.update = async (id, data) => {
+  await City
+    .findByIdAndUpdate(id, {
+      $set: {
+        ibgeCode: data.ibgeCode,
+        name: data.name,
+        ufCode: data.ufCode,
+        latitude: data.latitude,
+        longitude: data.longitude
+      }
+    })
+}
+
+exports.delete = async (id) => {
+  await City
+    .findByIdAndRemove(id)
+}
