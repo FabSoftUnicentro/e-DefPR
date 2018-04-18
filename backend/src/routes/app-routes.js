cccb6d432ad90e09aa3ed060b550de9c69d5d590
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-04-17 21:17:44 -0300

    Adicionado rotas de cidate e estado (city e state)

--- backend/src/routes/app-routes.js
+++ backend/src/routes/app-routes.js
@@ -3,7 +3,11 @@
 const express = require('express')
 const router = express.Router()
 const personRoutes = require('./person-routes')
+const stateRoutes = require('./state-routes')
+const cityRoutes = require('./city-routes')
 
 router.use('/person', personRoutes)
+router.use('/state', stateRoutes)
+router.use('/city', cityRoutes)
 
 module.exports = router
