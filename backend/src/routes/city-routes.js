471aa7413555c763f65908903e0aa4492b6d2876
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-04-24 21:29:34 -0300

    adicionado rota de busca de cidade por estado

--- backend/src/routes/city-routes.js
+++ backend/src/routes/city-routes.js
@@ -6,8 +6,9 @@ const controller = require('../controllers/city-controller')
 const authService = require('../services/auth-service')
 
 router.get('/', controller.get)
+router.get('/state/:id', controller.getByState)
 router.get('/:id', controller.getById)
-router.post('/', authService.authorize, controller.post)
+router.post('/', controller.post)
 router.put('/:id', authService.authorize, controller.put)
 router.delete('/:id', authService.authorize, controller.delete)
 
