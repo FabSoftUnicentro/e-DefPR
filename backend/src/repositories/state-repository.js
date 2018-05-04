b3547a5a17219496ae71d1d21de48c07f9339251
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-05-03 21:09:54 -0300

    refatorado update

--- backend/src/repositories/state-repository.js
+++ backend/src/repositories/state-repository.js
@@ -25,15 +25,15 @@ exports.create = async (data) => {
 }
 
 exports.update = async (id, data) => {
-  await State
-    .findByIdAndUpdate(id, {
-      $set: {
-        ibgeCode: data.ibgeCode,
-        name: data.name,
-        initials: data.initials
-      }
-    })
+  let state = await State.findById(id)
+
+  for (var key in data) {
+    state[key] = data[key]
+  }
+
+  await state.save()
 }
+
 
 exports.delete = async (id) => {
   await State
