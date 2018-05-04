46b5615db71abe100cb32b5adf08075cad523617
dav3sk <davibastos.v@gmail.com>
2018-05-03 22:23:50 -0300

    Remoção de console.log de teste

--- backend/src/services/encrypt-service.js
+++ backend/src/services/encrypt-service.js
@@ -6,8 +6,6 @@ exports.encryptPassword = function (password) {
   let hash = bcrypt.hash(password, saltRounds).then(function (hash) {
     return hash
   })
-  console.log('encryptPassword returns:')
-  console.log(hash)
   return hash
 }
 
@@ -15,6 +13,5 @@ exports.checkPassword = function (password, hash) {
   let result = bcrypt.compare(password, hash).then(function (res) {
     return res
   })
-
   return result
 }
