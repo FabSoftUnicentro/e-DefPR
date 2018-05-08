aa3523be8245587049227e15f1ab9b7d76fd099d
Alexandre Gueths <agueths@gmail.com>
2018-05-08 16:24:55 -0300

    Corrigindo metodo listByState fora da classe

--- frontend/src/services/CitiesService.js
+++ frontend/src/services/CitiesService.js
@@ -1,26 +1,27 @@
 import Service from './Service'
 
+
 class CityService extends Service {
   constructor () {
     super('/cities')
   }
-}
 
-/**
- * List all cities that belong to a specific state.
- * @param {UUID} state_uid
- */
-async listByState (stateUid) {
-  if (!stateUid) {
-    return []
+  /**
+   * List all cities that belong to a specific state.
+   * @param {UUID} state_uid
+   */
+  async listByState (stateUid) {
+    if (!stateUid) {
+      return []
+    }
+    let result = await fetcher.get(`${this.route}/state/${stateUid}`) /// TODO verify backend route
+
+    if (result.status !== 200) {
+      this.catchErrors(result)
+    }
+
+    return result
   }
-  let result = await fetcher.get(`${this.route}/state/${stateUid}`) /// TODO verify backend route
-
-  if (result.status !== 200) {
-    this.catchErrors(result)
-  }
-
-  return result
 }
 
 export default (new CityService())
