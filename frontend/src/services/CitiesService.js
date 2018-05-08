ddc08156e6d94a852cdfcd1b0d9b05d2bc8aae0d
Alexandre Gueths <agueths@gmail.com>
2018-05-08 17:59:14 -0300

    Retirado BasicSelect e CitySelect de Adapters; Adicionado CitiesService.listByState(stateUid)

--- frontend/src/services/CitiesService.js
+++ frontend/src/services/CitiesService.js
@@ -4,6 +4,23 @@ class CityService extends Service {
   constructor () {
     super('/cities')
   }
+
+  /**
+   * List all cities that belong to a specific state.
+   * @param {UUID} state_uid
+   */
+  async listByState (stateUid) {
+    if (!stateUid) {
+      return []
+    }
+    let result = await fetcher.get(`${this.route}/state/${stateUid}`) /// TODO verify backend route
+
+    if (result.status !== 200) {
+      this.catchErrors(result)
+    }
+
+    return result
+  }
 }
 
 export default (new CityService())
