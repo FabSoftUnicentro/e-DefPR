f69a1148c0826179191e5a7ed85831530e571c5e
dav3sk <davibastos.v@gmail.com>
2018-06-05 19:15:32 -0300

    Implementado migration de State

---
+++ backend/database/migrations/2018_05_20_235024_create_states_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 64)->unique();
+            $table->string('abbr', 2)->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
