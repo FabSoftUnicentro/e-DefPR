5f793d7b797243d97781498cd681b320ff6c0886
paulopieczarka <paulopieczarka@gmail.com>
2018-06-05 11:58:53 -0300

    Refatorado para utilizar es6.

---
+++ frontend/src/adapters/index.js
@@ -0,0 +1,7 @@
+export { default as CitySelectAdapter } from './CitySelectAdapter'
+export { default as CivilStateSelect } from './CivilStateSelect'
+export { default as DatePickerAdapter } from './DatePickerAdapter'
+export { default as GenderSelect } from './GenderSelect'
+export { default as SelectAdapter } from './SelectAdapter'
+export { default as TextFieldAdapter } from './TextFieldAdapter'
+export { default as VirtualizedSelectAdapter } from './VirtualizedSelectAdapter'
