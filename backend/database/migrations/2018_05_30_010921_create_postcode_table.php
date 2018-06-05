f4061f690c9bbb870da86381b3f3009aa149bc76
Gianluca Bine <gian_bine@hotmail.com>
2018-06-04 22:51:56 -0300

    Adequação nas migrations

--- backend/database/migrations/2018_05_30_010921_create_postcode_table.php
+++ backend/database/migrations/2018_05_30_010921_create_postcode_table.php
@@ -35,6 +35,6 @@ class CreatePostcodeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('postcode');
+        Schema::dropIfExists('postcodes');
     }
 }
