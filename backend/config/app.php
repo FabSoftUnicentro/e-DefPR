2c1c82031e3b65910c5b52d5d0715aa3f4321af5
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-06-13 15:07:04 -0300

    Criado permission provider

--- backend/config/app.php
+++ backend/config/app.php
@@ -146,11 +146,14 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
          */
 
+        Spatie\Permission\PermissionServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
