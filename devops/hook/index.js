da51ca002c431d61b58d88ec4444d3d9a093e75f
Gianluca Bine <gian_bine@hotmail.com>
2018-07-25 22:24:06 +0000

    Refatoração de código conforme review

--- devops/hook/index.js
+++ devops/hook/index.js
@@ -1,29 +1,27 @@
-var githubhook = require('githubhook');
-var github = githubhook({});
+var githubhook = require('githubhook')
+var github = githubhook({})
 
-const { exec } = require('child_process');
+const { exec } = require('child_process')
 
-github.listen();
+github.listen()
 
 github.on('pull_request:e-DefPR', function (ref, data) {
-    if (data.action == 'opened') {
+  if (data.action == 'opened') {
+    const url = data.pull_request.html_url
+    const message = '@here REVIEW: ' + url + '/files'
 
-        const url = data.pull_request.html_url;
-	const message = '@here REVIEW: ' + url + '/files';
+    exec('edef-slack-notify --review "' + message + '"', (err, stdout, stderr) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+  }
 
-	exec('edef-slack-notify --review "' + message + '"', (err, stdout, stderr) => {
-            if (err) {
-                console.log(err)
-            }
-        });
-    }
+  if (data.action == 'closed' && data.pull_request.merged) {
+    exec('edef-app-deploy --homolog', (err, stdout, stderr) => {
+      if (err) {
 
-    if (data.action == 'closed' && data.pull_request.merged) {
-        exec('edef-app-deploy --homolog', (err, stdout, stderr) => {
-            if (err) {
-                return;
-            }
-        });
-    }
-});
-
+      }
+    })
+  }
+})
