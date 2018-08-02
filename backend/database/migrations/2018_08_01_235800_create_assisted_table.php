222549e475b0144ca2f740237ac07cc9503aeeb5
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-08-01 21:56:59 -0300

    Correção no drop da tabela

--- backend/database/migrations/2018_08_01_235800_create_assisted_table.php
+++ backend/database/migrations/2018_08_01_235800_create_assisted_table.php
@@ -38,6 +38,6 @@ class CreateAssistedTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('assisted');
+        Schema::dropIfExists('assisteds');
     }
 }
