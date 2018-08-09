bdc53ba9ac4a0459b83515b7679ebd75b4fe3e62
dav3sk <davibastos.v@gmail.com>
2018-08-09 19:14:00 -0300

    Adicionado paginator ao Role controller.

--- backend/app/Http/Controllers/Api/RoleController.php
+++ backend/app/Http/Controllers/Api/RoleController.php
@@ -12,12 +12,14 @@ use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
+    private $itemsPerPage = 10;
+
     /**
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        $roles= Role::all();
+        $roles= Role::paginate($this->itemsPerPage);
 
         return RoleResource::collection($roles);
     }
