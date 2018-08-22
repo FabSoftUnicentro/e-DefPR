acfbc69d1fb1f013f9790598a724975e7ee00396
HigorG <higorgardin@hotmail.com>
2018-08-22 20:33:01 -0300

    Exibindo lista de usuarios

--- frontend/src/app/services/Assisted.js
+++ frontend/src/app/services/Assisted.js
@@ -8,7 +8,7 @@ class Assisted extends Service {
       if (result.statusCode === 'SUCCESS') {
         return result
       } 
-      
+
       return result
     } catch (error) {
       throw error
