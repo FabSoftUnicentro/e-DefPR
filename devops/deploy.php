c66e05bff53b4a2b35cd2d3ff7f5a892fa8ad33b
Gianluca Bine <gian_bine@hotmail.com>
2018-09-07 16:15:36 -0300

    Desativado GIT tty desnecessario

--- devops/deploy.php
+++ devops/deploy.php
@@ -168,7 +168,7 @@ set('application', 'e-DefPR');
 set('repository', 'git@github.com:C3DSU/e-DefPR.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+set('git_tty', false);
 
 // Shared files/dirs between deploys
 add('shared_files', []);
