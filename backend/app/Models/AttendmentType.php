10603a4904b3068fa6132f54c9faa233ef94e993
dav3sk <davibastos.v@gmail.com>
2018-09-19 19:14:45 -0300

    Implementa model de AttendmnetType

---
+++ backend/app/Models/AttendmentType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AttendmentType extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated-at'
+    ];
+}
