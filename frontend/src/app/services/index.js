e1b4004b68889d63b2806effde68a0d763b05ae6
HigorG <higorgardin@hotmail.com>
2018-09-20 18:15:40 -0300

    Adicionado service de permissão no index

--- frontend/src/app/services/index.js
+++ frontend/src/app/services/index.js
@@ -2,12 +2,14 @@ import Authentication from './Authentication'
 import User from './User'
 import Assisted from './Assisted'
 import Role from './Role'
+import Permission from './Permission'
 import Location from './Location'
 
 const authentication = new Authentication()
 const userService = new User()
 const assistedService = new Assisted()
 const roleService = new Role()
+const permissionService = new Permission()
 const location = new Location()
 
 export {
@@ -15,5 +17,6 @@ export {
   userService,
   assistedService,
   roleService,
+  permissionService,
   location
 }
