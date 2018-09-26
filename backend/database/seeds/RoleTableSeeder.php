707c6f229c5a2c75f6f57f04e7327ed50a2332be
dav3sk <davibastos.v@gmail.com>
2018-09-26 20:30:18 -0300

    Adiciona permissões para Tipo de Atendimento

--- backend/database/seeds/RoleTableSeeder.php
+++ backend/database/seeds/RoleTableSeeder.php
@@ -74,7 +74,11 @@ class RoleTableSeeder extends Seeder
             'list-permission',
             'register-permission',
             'update-permission',
-            'delete-permission'
+            'delete-permission',
+            'list-attendmentType',
+            'register-attendmentType',
+            'update-attendmentType',
+            'delete-attendmentType'
         ];
         foreach ($permissions as $permission) {
             Permission::create(['name' => $permission]);
