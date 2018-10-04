02b117fd83677bf6176eadf5babf044078671371
EnViKeyy <pierre__jp@hotmail.com>
2018-10-03 22:23:15 -0300

    Movido request de store e update para pasta AttendmentType

--- backend/app/Http/Requests/AttendmentType/UpdateRequest.php
+++ backend/app/Http/Requests/AttendmentType/UpdateRequest.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\AttendmentType;
 
-class AttendmentTypeUpdateRequest extends BaseRequest
+use App\Http\Requests\BaseRequest;
+
+class UpdateRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
