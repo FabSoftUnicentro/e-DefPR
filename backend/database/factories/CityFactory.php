3f7ab1d09ae1baa08247c60da8afcbdc8ebdf352
Gianluca Bine <gian_bine@hotmail.com>
2018-10-10 20:55:00 -0300

    Correção em migrations e seeders

---
+++ backend/database/factories/CityFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB as DB;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Models\City::class, function (Faker $faker) {
+    return [
+        'name' => $faker->domainName,
+        'state_id' => factory(App\Models\State::class),
+    ];
+});
