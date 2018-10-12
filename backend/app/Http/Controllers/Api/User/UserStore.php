7871a379da4849af20a6c34f3bbbc158bbe7e748
Gianluca Bine <gian_bine@hotmail.com>
2018-10-12 20:39:14 -0300

    Refatoração de código

--- backend/app/Http/Controllers/Api/User/UserStore.php
+++ backend/app/Http/Controllers/Api/User/UserStore.php
@@ -6,29 +6,21 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\User\StoreRequest;
 use App\Models\User;
 use App\Http\Resources\User as UserResource;
-use Illuminate\Http\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 class UserStore extends Controller
 {
     /**
      * @param StoreRequest $request
-     * @return UserResource|JsonResponse
+     * @return UserResource
      * @throws \Throwable
      */
     public function __invoke(StoreRequest $request)
     {
         /** @var User $user */
-        $user =  new User($request->all());
+        $user = new User($request->all());
 
-        try {
-            $user->saveOrFail();
+        $user->saveOrFail();
 
-            return new UserResource($user);
-        } catch (\Exception $e) {
-            return JsonResponse::create([
-                'message' => $e->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
-        }
+        return new UserResource($user);
     }
 }
