312ff719553e0c0fc8f6cf3b0c423b2297671e6a
Gianluca Bine <gian_bine@hotmail.com>
2018-10-12 22:38:55 -0300

    Adequação de uso de single action controllers de State

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -50,17 +50,17 @@ Route::prefix('user')->group(function () {
 
 // State routes
 Route::prefix('state')->group(function () {
-    Route::get('/', 'Api\StateController@index')->middleware('auth:api');
-    Route::get('/{id}', 'Api\StateController@show')->middleware('auth:api');
+    Route::get('/', 'Api\State\StateList')->middleware('auth:api');
+    Route::get('/{state}', 'Api\State\StateShow')->middleware('auth:api');
 
     Route::group(['middleware' => ['permission:register-state']], function () {
-        Route::post('/', 'Api\StateController@store')->middleware('auth:api');
+        Route::post('/', 'Api\State\StateStore')->middleware('auth:api');
     });
     Route::group(['middleware' => ['permission:update-state']], function () {
-        Route::put('/{id}', 'Api\StateController@update')->middleware('auth:api');
+        Route::put('/{state}', 'Api\State\StateUpdate')->middleware('auth:api');
     });
     Route::group(['middleware' => ['permission:delete-state']], function () {
-        Route::delete('/{id}', 'Api\StateController@destroy')->middleware('auth:api');
+        Route::delete('/{state}', 'Api\State\StateDestroy')->middleware('auth:api');
     });
 });
 
