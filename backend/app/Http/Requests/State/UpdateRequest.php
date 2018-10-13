9ba54a5dadd8be5a36eb803b9cb5a8fbdd08d639
Gianluca Bine <gian_bine@hotmail.com>
2018-10-12 22:36:24 -0300

    Adequação em regra de validação

--- backend/app/Http/Requests/State/UpdateRequest.php
+++ backend/app/Http/Requests/State/UpdateRequest.php
@@ -24,21 +24,8 @@ class UpdateRequest extends BaseRequest
     public function rules()
     {
         return [
-            'name' => 'required|string',
-            'abbr' => 'required|string'
-        ];
-    }
-
-    /**
-     * Filters to be applied to the input.
-     *
-     * @return array
-     */
-    public function filters()
-    {
-        return [
-            'name' => 'trim|escape',
-            'abbr' => 'trim|escape'
+            'name' => 'string',
+            'abbr' => 'string'
         ];
     }
 }
