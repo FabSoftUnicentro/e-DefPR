e9fc3def620ab7bc066c91e7dce776d0e1e099bc
EnViKeyy <pierre__jp@hotmail.com>
2018-10-17 18:41:47 -0300

    Implementado Single Action Controllers do metodo UnassignPermission

--- backend/app/Http/Controllers/Api/Role/RoleUnassignPermission.php
+++ backend/app/Http/Controllers/Api/Role/RoleUnassignPermission.php
@@ -1,15 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 17/10/2018
- * Time: 18:22
- */
 
 namespace App\Http\Controllers\Api\Role;
 
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
+use App\Http\Resources\Role as RoleResource;
 
-class RoleUnassignPermission
+class RoleUnassignPermission extends Controller
 {
+    public function __invoke(Role $role, $permission)
+    {
+        $role->revokePermissionTo($permission);
 
+        return new RoleResource($role);
+    }
 }
