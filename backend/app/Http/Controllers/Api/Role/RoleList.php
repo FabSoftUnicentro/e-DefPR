38e690545f9246073176ee4e1f186473fbffddde
EnViKeyy <pierre__jp@hotmail.com>
2018-10-17 18:40:57 -0300

    Implementado Single Action Controllers do metodo List

--- backend/app/Http/Controllers/Api/Role/RoleList.php
+++ backend/app/Http/Controllers/Api/Role/RoleList.php
@@ -1,15 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 17/10/2018
- * Time: 17:46
- */
 
 namespace App\Http\Controllers\Api\Role;
 
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
+use App\Http\Resources\Role as RoleResource;
 
-class RoleList
+class RoleList extends Controller
 {
+    private $itemsPerPage = 10;
 
+    /**
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function __invoke()
+    {
+        $roles= Role::paginate($this->itemsPerPage);
+
+        return RoleResource::collection($roles);
+    }
 }
