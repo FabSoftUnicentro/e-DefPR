eaecd018bd29b0db631204fd3dc2c327151ddee2
EnViKeyy <pierre__jp@hotmail.com>
2018-10-17 18:41:13 -0300

    Implementado Single Action Controllers do metodo Show

--- backend/app/Http/Controllers/Api/Role/RoleShow.php
+++ backend/app/Http/Controllers/Api/Role/RoleShow.php
@@ -1,15 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 17/10/2018
- * Time: 17:55
- */
 
 namespace App\Http\Controllers\Api\Role;
 
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
+use App\Http\Resources\Role as RoleResource;
 
-class RoleShow
+class RoleShow extends Controller
 {
-
+    public function __invoke(Role $role)
+    {
+        return new RoleResource($role);
+    }
 }
