a5f0cc66d17900fb7f6202740e8b8e2860036662
EnViKeyy <pierre__jp@hotmail.com>
2018-10-18 10:57:05 -0300

    Implementado teste dos métodos assignPermission e assignPermissions

--- backend/tests/Feature/Role/UpdateTest.php
+++ backend/tests/Feature/Role/UpdateTest.php
@@ -1,51 +1,15 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Jean Pierri
+ * Date: 17/10/2018
+ * Time: 21:48
+ */
 
-namespace Tests\Feature\Assisted;
+namespace Tests\Feature\Role;
 
-use App\Models\Assisted;
-use App\Models\User;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class UpdateTest extends TestCase
+class UpdateTest
 {
-    use RefreshDatabase;
 
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->artisan('db:seed', ['--class' => 'RoleTableSeeder']);
-    }
-
-    /**
-     * @test Update a specific assisted
-     */
-    public function testUpdate()
-    {
-        $admin = factory(User::class)->create();
-
-        $admin->assignRole('master');
-
-        $assisted = factory(Assisted::class)->create([
-            'name' => 'Test 1'
-        ]);
-
-        $response1 = $this->actingAs($admin)->get('/assisted/' . $assisted->id);
-
-        $response1->assertSuccessful();
-
-        $this->assertEquals('Test 1', $response1->json()['data']['name']);
-
-        $response2 = $this->actingAs($admin)->put('/assisted/' . $assisted->id, [
-            'name' => 'Test 2'
-        ]);
-
-        $response2->assertSuccessful();
-
-        $response3 = $this->actingAs($admin)->get('/assisted/' . $assisted->id);
-
-        $response3->assertSuccessful();
-
-        $this->assertEquals('Test 2', $response3->json()['data']['name']);
-    }
 }
