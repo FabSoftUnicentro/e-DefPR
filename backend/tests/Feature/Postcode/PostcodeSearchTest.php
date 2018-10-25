ad5e3c5f9c1519622cc16ba62ee47ad0932bdc2e
Gianluca Bine <gian_bine@hotmail.com>
2018-10-24 21:28:26 -0300

    Implementado testes funcionais de postcode

---
+++ backend/tests/Feature/Postcode/PostcodeSearchTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Postcode;
+
+use App\Models\Postcode;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\Resources\Postcode as PostcodeResource;
+
+class PostcodeSearchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+	/**
+     * @test Search a postcode that already exists
+     */
+    public function testPostcodeSearchResource()
+    {
+    	$address = factory(Postcode::class)->create();
+
+        $response =  $this->get('/postcode/' . $address->postcode);
+
+        $response->assertResource(PostcodeResource::make($address));
+    }
+
+    /**
+     * @test Search a postcode
+     */
+    public function testPostcodeSearch()
+    {
+    	$postcode = "85015-310";
+
+    	$response = $this->get('/postcode/' . $postcode);
+
+    	$response->assertSuccessful();
+    }
+
+    /**
+     * @test Search a inexistent postcode
+     */
+    public function testInexistentPostcodeSearch()
+    {
+    	$postcode = "00000-000";
+
+    	$response = $this->get('/postcode/' . $postcode);
+
+    	$response->assertNotFound();
+    }
+}
