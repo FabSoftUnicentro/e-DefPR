bb1822519f410ccc32a53c05fba0d55e8120f365
HigorG <higorgardin@hotmail.com>
2018-10-31 18:01:23 -0300

    Implementado associação de permissões a nivel de acesso

--- frontend/src/app/pages/role/RoleAssign.js
+++ frontend/src/app/pages/role/RoleAssign.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react'
 import Page from '../../components/page/Page'
 import Form from '../../components/form/Form'
 import message from 'antd/lib/message'
-import Icon from 'antd/lib/icon'
-import Button from 'antd/lib/button'
-import { Cascader, Transfer, Row, Col } from 'antd';
+import { Cascader, Row, Col, Checkbox, Button, Icon } from 'antd'
 import { roleService } from '../../services'
-import * as yup from 'yup'
 import { Redirect } from 'react-router-dom'
 
 class RoleAssign extends Component {
@@ -15,55 +12,29 @@ class RoleAssign extends Component {
 
     this.state = {
       data: undefined,
+      permissionList: undefined,
+      permissions: undefined,
+      selectedRole: undefined,
       total: 0,
       visible: false,
       redirect: false,
       selected: {
         name: ''
-      },
-
-      mockData: [],
-      targetKeys: [],
+      }
     }
 
-    this.onSubmit = this.onSubmit.bind(this)
+    this.onClick = this.onClick.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.onChangePermissionState = this.onChangePermissionState.bind(this)
   }
 
-  componentWillMount () {
-    console.log("montou")
-    roleService.list()
-      .then(({ data, meta }) => {
-        data = data.map(item => ({ value: item.id, label: item.name }))
-        this.setState({ data, total: meta.total })
-      })
-      .catch(() => message.error('Não foi possível acessar os níveis de acesso.'))
-  }
-
-  componentDidMount () {
-    this.getMock();
-  }
-
-  async onSubmit (values) {
-    const removeCreatingMessage = message.loading('Cadastrando nível de acesso', 0)
-
+  async componentDidMount () {
     try {
-      const result = await roleService.create(values)
-
-      if (result.status === 201) {
-        message.success('Nível de acesso cadastrado com sucesso!')
-
-        this.setState({
-          redirect: true
-        })
-      } else if (result.status === 403) {
-        message.error('Preencha corretamente o nome!')
-      } else {
-        return message.error('Não foi possível cadastrar o nível de acesso!', 2)
-      }
+      const { data, meta } = await roleService.list()
+      const mappedData = data.map(item => ({ value: item.id, label: item.name }))
+      this.setState({ data: mappedData, total: meta.total })
     } catch (error) {
-      return message.error('Erro inesperado, tente novamente!', 2)
-    } finally {
-      removeCreatingMessage()
+      message.error('Não foi possível acessar os níveis de acesso.')
     }
   }
 
@@ -71,37 +42,51 @@ class RoleAssign extends Component {
     this.setState({ alertProps: { type, message } })
   }
 
-  onChange(values, selectedOptions) {
-    console.log(values);
-  }
-
-  getMock = () => {
-    const targetKeys = [];
-    const mockData = [];
-    for (let i = 0; i < 10; i++) {
-      const data = {
-        key: i.toString(),
-        title: `content${i + 1}`,
-        chosen: Math.random() * 2 > 1,
-      };
-      if (data.chosen) {
-        targetKeys.push(data.key);
-      }
-      mockData.push(data);
+  async onChange (values, selectedOptions) {
+    try {
+      this.setState({ permissionList: undefined })
+      const [ id ] = values
+      const data = await roleService.listPermissions(id)
+      const permissionList = Object.values(data).map(item => ({ key: item.id, title: item.name, chosen: item.chosen }))
+      this.setState({ selectedRole: id, permissionList })
+    } catch (error) {
+      message.error('Não foi possível acessar as informações dos funcionários.')
     }
-    this.setState({ mockData, targetKeys });
   }
 
-  handleChange = (targetKeys, direction, moveKeys) => {
-    console.log(targetKeys)
-    console.log(direction)
-    console.log(moveKeys)
-    this.setState({ targetKeys });
+  async onChangePermissionState (checkedValue) {
+    const removeAssignMessage = message.loading('Associando permissão a nível de acesso', 0)
+
+    const { selectedRole } = this.state
+    const { target: { value, checked } } = checkedValue
+
+    try {
+      const result = checked ? await roleService.assignPermission(selectedRole, value) : await roleService.unassignPermission(selectedRole, value)
+
+      if (result.status === 200) {
+        message.success('Associado permissão a nível de acesso com sucesso!')
+      } else {
+        return message.error('Não foi possível associar permissão a nível de acesso!', 2)
+      }
+    } catch (error) {
+      return message.error('Erro inesperado, tente novamente!', 2)
+    } finally {
+      removeAssignMessage()
+    }
   }
 
+  handleChange (targetKeys, direction, moveKeys) {
+    this.setState({ targetKeys })
+  }
+
+  onClick () {
+    this.setState({
+      redirect: true
+    })
+  }
 
   render () {
-    const { data, total } = this.state
+    const { data, permissionList } = this.state
     if (this.state.redirect === true) {
       return <Redirect to='/role' />
     }
@@ -110,33 +95,23 @@ class RoleAssign extends Component {
       <Page.Header />
 
       <Page.Context>
-        <h2>Associar nível de acesso a permissões</h2>
+        <h2>Associar permissões a nível de acesso</h2>
 
         <div className='app-page-box'>
           <Form
             onSubmit={this.onSubmit}
-            >
-            <Form.Step title='Informações do nível de acesso'>
-              <label>Escolha um nível de acesso:</label>
-              <Cascader style={{marginTop: 5, marginBottom: 20}} options={data} onChange={this.onChange} placeholder="Escolha um nível de acesso" />
+          >
+            <label>Escolha um nível de acesso e marque as permissões que deseja associar:</label>
+            <Cascader style={{ marginTop: 5, marginBottom: 20 }} options={data} onChange={this.onChange} placeholder='Escolha um nível de acesso' />
 
-              <Transfer
-                titles={['Inativas','Ativas']}
-                style={{marginTop: 5, marginBottom: 20}}
-                dataSource={this.state.mockData}
-                showSearch
-                listStyle={{
-                  width: 250,
-                  height: 300,
-                }}
-                operations={['Ativar', 'Desativar']}
-                targetKeys={this.state.targetKeys}
-                onChange={this.handleChange}
-                render={item => `${item.title}`}
-                //footer={this.renderFooter}
-              />
-            </Form.Step>
-            <Button size='large'type='primary'htmlType='submit'>Salvar <Icon type='check' /></Button>
+            <Row>
+              { permissionList && permissionList.map(item => (
+                <Col span={8} >
+                  <Checkbox defaultChecked={item.chosen} value={item.key} onChange={this.onChangePermissionState}>{item.title}</Checkbox>
+                </Col>
+              )) }
+            </Row>
+            <Button style={{ marginTop: 15 + 'px' }} size='large' type='primary' onClick={this.onClick} htmlType='submit'>Encerrar edição<Icon type='check' /></Button>
           </Form>
         </div>
       </Page.Context>
