4c8e48a6572dd5ef841bd3980db846dcee578522
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-10-31 22:15:20 -0300

    Adicionado get sem paginate

--- backend/app/Http/Controllers/Api/State/StateList.php
+++ backend/app/Http/Controllers/Api/State/StateList.php
@@ -5,18 +5,26 @@ namespace App\Http\Controllers\Api\State;
 use App\Http\Controllers\Controller;
 use App\Models\State;
 use App\Http\Resources\State as StateResource;
+use Illuminate\Http\Request;
 
 class StateList extends Controller
 {
     /** @var int */
-
+    private $itemsPerPage = 10;
 
     /**
+     * @param Request $request
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $states = State::orderBy('abbr', 'asc')->get();
+        $paginate = intval($request->get('paginate', 1));
+
+        if ($paginate === 1) {
+            $states = State::orderBy('abbr', 'asc')->paginate($this->itemsPerPage);
+        } else {
+            $states = State::orderBy('abbr', 'asc')->get();
+        }
 
         return StateResource::collection($states);
     }
