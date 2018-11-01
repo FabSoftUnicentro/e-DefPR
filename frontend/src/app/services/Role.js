ba2e0e07a31c58d901fa32af64f48d4ca9b96edc
HigorG <higorgardin@hotmail.com>
2018-10-31 21:05:03 -0300

    Adicionado serviço de associação de permissões

--- frontend/src/app/services/Role.js
+++ frontend/src/app/services/Role.js
@@ -8,6 +8,18 @@ class Role extends Service {
   async list (page = '1') {
     return this.get(`/role?page=${page}`)
   }
+
+  async listPermissions (roleId) {
+    return this.get(`/role/${roleId}/permissions`)
+  }
+
+  async assignPermission (roleId, permissionId) {
+    return this.put(`/role/${roleId}/assign-permission/${permissionId}`)
+  }
+
+  async unassignPermission (roleId, permissionId) {
+    return this.put(`/role/${roleId}/unassign-permission/${permissionId}`)
+  }
 }
 
 export default Role
