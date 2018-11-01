d0ca92b42647efb42c8a3fb2d4e8472d9f8e236d
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-10-31 21:46:50 -0300

    Removido Paginate

--- backend/app/Http/Controllers/Api/Role/RoleList.php
+++ backend/app/Http/Controllers/Api/Role/RoleList.php
@@ -8,14 +8,13 @@ use App\Http\Resources\Role as RoleResource;
 
 class RoleList extends Controller
 {
-    private $itemsPerPage = 10;
 
     /**
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function __invoke()
     {
-        $roles= Role::paginate($this->itemsPerPage);
+        $roles= Role::all();
 
         return RoleResource::collection($roles);
     }
