bb411f106f184b102a46f9f3e1f0358a62578b04
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-10-31 22:15:10 -0300

    Adicionado get sem paginate

--- backend/app/Http/Controllers/Api/Role/RoleList.php
+++ backend/app/Http/Controllers/Api/Role/RoleList.php
@@ -3,18 +3,28 @@
 namespace App\Http\Controllers\Api\Role;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use App\Http\Resources\Role as RoleResource;
 
 class RoleList extends Controller
 {
+    /** @var int */
+    private $itemsPerPage = 10;
 
     /**
+     * @param Request $request
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $roles= Role::all();
+        $paginate = intval($request->get('paginate', 1));
+
+        if ($paginate === 1) {
+            $roles = Role::paginate($this->itemsPerPage);
+        } else {
+            $roles= Role::all();
+        }
 
         return RoleResource::collection($roles);
     }
