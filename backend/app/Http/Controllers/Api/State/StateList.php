876fd7c1ba789c6988558603ee317d596444b0df
Enrique Augusto da Roza <enriqueaugroza@gmail.com>
2018-10-31 21:47:00 -0300

    Removido Paginate

--- backend/app/Http/Controllers/Api/State/StateList.php
+++ backend/app/Http/Controllers/Api/State/StateList.php
@@ -9,14 +9,14 @@ use App\Http\Resources\State as StateResource;
 class StateList extends Controller
 {
     /** @var int */
-    private $itemsPerPage = 10;
+
 
     /**
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function __invoke()
     {
-        $states = State::orderBy('abbr', 'asc')->paginate($this->itemsPerPage);
+        $states = State::orderBy('abbr', 'asc')->get();
 
         return StateResource::collection($states);
     }
