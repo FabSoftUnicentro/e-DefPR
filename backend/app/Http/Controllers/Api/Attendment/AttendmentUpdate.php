2e207cb45b9ecaa6ada2fdaca0757f672197def6
EnViKeyy <pierre__jp@hotmail.com>
2018-11-07 17:42:41 -0200

    Implementado single controller action do método Update

--- backend/app/Http/Controllers/Api/Attendment/AttendmentUpdate.php
+++ backend/app/Http/Controllers/Api/Attendment/AttendmentUpdate.php
@@ -1,15 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 01/11/2018
- * Time: 15:18
- */
 
 namespace App\Http\Controllers\Api\Attendment;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Attendment\UpdateRequest;
+use App\models\Attendment;
+use App\Http\Resources\Attendment as AttendmentResource;
 
-class AttendmentUpdate
+class AttendmentUpdate extends Controller
 {
+    /**
+     * @param UpdateRequest $request
+     * @param Attendment $attendment
+     * @return AttendmentResource
+     * @throws \Throwable
+     */
+    public function __invoke(UpdateRequest $request, Attendment $attendment)
+    {
+        $attendment->update($request->all());
 
+        $attendment->saveOrFail();
+
+        return new AttendmentResource($attendment);
+    }
 }
