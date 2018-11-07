6b6535c6be7195e4d033b5b047c77b5561fa9251
EnViKeyy <pierre__jp@hotmail.com>
2018-11-07 17:41:59 -0200

    Implementado single controller action do método Store

--- backend/app/Http/Controllers/Api/Attendment/AttendmentStore.php
+++ backend/app/Http/Controllers/Api/Attendment/AttendmentStore.php
@@ -1,15 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 01/11/2018
- * Time: 15:12
- */
 
 namespace App\Http\Controllers\Api\Attendment;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Attendment\StoreRequest;
+use App\Models\Attendment;
+use App\Http\Resources\Attendment as AttendmentResource;
 
-class AttendmentStore
+class AttendmentStore extends Controller
 {
+    /**
+     * @param StoreRequest $request
+     * @return AttendmentResource
+     * @throws \Throwable
+     */
+    public function __invoke(StoreRequest $request)
+    {
+        $attendment = new Attendment($request->all());
 
+        $attendment->saveOrFail();
+
+        return new AttendmentResource($attendment);
+    }
 }
