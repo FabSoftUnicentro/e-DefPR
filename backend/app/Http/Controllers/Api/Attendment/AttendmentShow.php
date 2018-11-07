08b36b1025e18aa65f5d4b149a7e87338260dbfe
EnViKeyy <pierre__jp@hotmail.com>
2018-11-07 17:41:41 -0200

    Implementado single controller action do método Show

--- backend/app/Http/Controllers/Api/Attendment/AttendmentShow.php
+++ backend/app/Http/Controllers/Api/Attendment/AttendmentShow.php
@@ -1,15 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 01/11/2018
- * Time: 15:16
- */
 
 namespace App\Http\Controllers\Api\Attendment;
 
+use App\Http\Controllers\Controller;
+use App\Models\Attendment;
+use App\Http\Resources\Attendment as AttendmentResource;
 
-class AttendmentShow
+class AttendmentShow extends Controller
 {
-
+    /**
+     * @param Attendment $attendment
+     * @return AttendmentResource
+     */
+    public function __invoke(Attendment $attendment)
+    {
+        return new AttendmentResource($attendment);
+    }
 }
