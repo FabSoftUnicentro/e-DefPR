df7f02c0314234e5646ba6f3ad7803b4b8193536
dav3sk <davibastos.v@gmail.com>
2018-11-07 19:20:05 -0200

    Implementa factory de AttendmentType

---
+++ backend/database/factories/AttendmentTypeFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Models\AttendmentType::class, function (Faker $faker) {
+    return [
+        'name' => $faker->unique()->name,
+        'description' => $faker->unique()->sentence
+    ];
+});
