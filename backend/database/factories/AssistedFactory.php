cadb4667743384a0362690267f261e264837ae99
EnViKeyy <pierre__jp@hotmail.com>
2018-11-09 11:16:42 -0200

    refatoração de código

--- backend/database/factories/AssistedFactory.php
+++ backend/database/factories/AssistedFactory.php
@@ -1,7 +1,8 @@
 <?php
 
 use Faker\Generator as Faker;
-use Illuminate\Support\Facades\DB as DB;
+use App\Models\Assisted;
+use App\Models\City;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +15,13 @@ use Illuminate\Support\Facades\DB as DB;
 |
 */
 
-$factory->define(App\Models\Assisted::class, function (Faker $faker) {
+$factory->define(Assisted::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'cpf' => $faker->numberBetween(999999999),
         'birth_date' => $faker->date(),
-        'birthplace' => DB::table('cities')->inRandomOrder()->first()->id,
+        'birthplace' => factory(City::class)->create(),
         'rg' => $faker->unique()->text(11),
         'rg_issuer' => 'SSP',
         'gender' => 'M',
