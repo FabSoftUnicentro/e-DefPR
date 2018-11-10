2e98162d1073129dfdb22c3ee2652f89431f3fb7
EnViKeyy <pierri.nvk@outlook.com>
2018-11-10 05:13:30 -0200

    Refatoração de código

--- backend/database/seeds/CounterPartTableSeeder.php
+++ backend/database/seeds/CounterPartTableSeeder.php
@@ -12,6 +12,6 @@ class CounterPartTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Models\CounterPart::class, 10)->create();
+        factory(CounterPart::class, 10)->create();
     }
 }
