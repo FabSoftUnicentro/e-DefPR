3b354f73b087957f6cd29234ce35db2c56271520
EnViKeyy <pierri.nvk@outlook.com>
2018-11-10 13:57:09 -0200

    Refatoração de código

--- backend/database/factories/AssistedFactory.php
+++ backend/database/factories/AssistedFactory.php
@@ -1,7 +1,9 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB as DB;
 use App\Models\Assisted;
+use App\Models\City;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +22,7 @@ $factory->define(Assisted::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'cpf' => $faker->numberBetween(999999999),
         'birth_date' => $faker->date(),
-        'birthplace' => '1100056',
+        'birthplace' => DB::table('cities')->exists() ? DB::table('cities')->inRandomOrder()->first()->id : factory(City::class)->create(),
         'rg' => $faker->unique()->text(11),
         'rg_issuer' => 'SSP',
         'gender' => 'M',
