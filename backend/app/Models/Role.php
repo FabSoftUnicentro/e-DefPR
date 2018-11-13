9a7e95627a5ff7f1854aa3167a30df11c3f54838
EnViKeyy <pierri.nvk@outlook.com>
2018-11-13 11:20:09 -0200

    Implementação do campo description em permissions

---
+++ backend/app/Models/Role.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Guard;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Exceptions\RoleAlreadyExists;
+ 
+class CustomRole extends Role
+{
+    public $guarded = ['id'];
+
+    public function __construct(array $attributes = [])
+    {
+        $attributes['guard_name'] = $attributes['guard_name'] ?? config('auth.defaults.guard');
+        $attributes['description'] = $attributes['description'] ?? config('auth.default.guard');
+
+        parent::__construct($attributes);
+
+        $this->setTable(config('permission.table_names.roles'));
+    }
+
+    public static function create(array $attributes = [])
+    {
+        $attributes['guard_name'] = $attributes['guard_name'] ?? Guard::getDefaultName(static::class);
+        $attributes['description'] = $attributes['description'] ?? Guard::getDefaultName(static::class);
+
+        if (static::where('name', $attributes['name'])->where('guard_name', $attributes['guard_name'])->first()) {
+            throw RoleAlreadyExists::create($attributes['name'], $attributes['description'], $attributes['guard_name']);
+        }
+
+        if (isNotLumen() && app()::VERSION < '5.4') {
+            return parent::create($attributes);
+        }
+
+        return static::query()->create($attributes);
+    }
+}
